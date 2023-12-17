f05ea90ca9de3cd0489fe3946f58268ffcdc2ade
DevKing472 <Fundorashweta472@gmail.com>
2023-12-17 12:26:02 +0000

    init base site

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <RouterView v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </RouterView>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        AboutView
+    </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style scoped>
+
+</style>
