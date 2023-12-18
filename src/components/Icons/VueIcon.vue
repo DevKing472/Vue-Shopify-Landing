855019aab189cb79e710fdbe0fa6d94bf25bf93e
DevKing472 <Fundorashweta472@gmail.com>
2023-12-18 18:26:16 +0000

    fix grabbing while hovering

--- src/components/Icons/VueIcon.vue
+++ src/components/Icons/VueIcon.vue
@@ -1,32 +1,32 @@
 <template>
-  <svg
-    v-drag="dragHandler"
-    ref="icon"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 261.76 226.69"
-    v-motion
-    :initial="{ y: 0 }"
-    :enter="{
-      y: 2,
-      transition: {
-        type: 'spring',
-        stiffness: 5,
-        damping: 0,
-        bounce: 0,
-        repeat: Infinity,
-        repeatType: 'mirror'
-      }
-    }"
-  >
-    <path
-      d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z"
-      fill="#41b883"
-    />
-    <path
-      d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z"
-      fill="#34495e"
-    />
-  </svg>
+  <div v-drag="dragHandler" ref="icon">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 261.76 226.69"
+      v-motion
+      :initial="{ y: 0 }"
+      :enter="{
+        y: 2,
+        transition: {
+          type: 'spring',
+          stiffness: 5,
+          damping: 0,
+          bounce: 0,
+          repeat: Infinity,
+          repeatType: 'mirror'
+        }
+      }"
+    >
+      <path
+        d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z"
+        fill="#41b883"
+      />
+      <path
+        d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z"
+        fill="#34495e"
+      />
+    </svg>
+  </div>
 </template>
 
 <script setup lang="ts">
