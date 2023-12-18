041dfc04a67995992860bf0e61ad1b35746305e4
DevKing472 <Fundorashweta472@gmail.com>
2023-12-18 19:49:56 +0000

    Add feature list

---
+++ src/components/Home/Features/FeatureList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="feature-container">
+    <div class="grid-item" v-for="(card, index) in cardData" :key="index">
+      <UCard>
+        <template #icon>
+          <component :is="card.icon" size="32" weight="thin"></component>
+        </template>
+        <template #header>
+          <h4>{{ card.header }}</h4>
+        </template>
+        <p>
+          {{ card.body }}
+        </p>
+      </UCard>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PhSparkle, PhAlien } from '@phosphor-icons/vue'
+const cardData = [
+  {
+    icon: PhAlien,
+    header: 'Vue 3',
+    body: 'Vue 3 is the latest version of Vue.js. It brings a lot of new features and performance improvements.'
+  },
+  {
+    icon: PhSparkle,
+    header: 'Vue Router',
+    body: 'Vue Router is the official router for Vue.js. It deeply integrates with Vue.js core to make building Single Page Applications with Vue.js a breeze.'
+  },
+  {
+    icon: PhAlien,
+    header: 'Pinia',
+    body: 'Pinia is a modern, typed and performant Store for Vue.'
+  }
+]
+</script>
+
+<style scoped></style>
--- src/components/Home/HomeFeatures.vue
+++ src/components/Home/HomeFeatures.vue
@@ -1,9 +1,42 @@
 <template>
   <section>
-    <div class="main-container">HomeFeatures</div>
+    <div class="main-container">
+      <div class="heading-container">
+        <a href="#">
+          <UButton label="Many Features, Wow" :type="'fancy'" size="small">
+            <template #icon>
+              <PhSparkle color="#53feed" />
+            </template>
+          </UButton>
+        </a>
+        <h3 style="margin-top: 1rem; margin-bottom: 0">What's in This Template?</h3>
+        <p>All you need to build a shopify app</p>
+      </div>
+      <FeatureList />
+    </div>
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { PhSparkle } from '@phosphor-icons/vue'
+import FeatureList from './Features/FeatureList.vue'
+</script>
 
-<style scoped></style>
+<style scoped>
+section {
+  background: linear-gradient(to bottom, var(--bleed-in), var(--primary));
+  padding-block: 2rem;
+}
+.heading-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 2rem;
+}
+.feature-container {
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+}
+</style>
