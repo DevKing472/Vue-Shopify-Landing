cf47f430827bb984a9a22080f31d5036c60fdf91
DevKing472 <Fundorashweta472@gmail.com>
2023-12-18 19:49:51 +0000

    Add uCard

---
+++ src/components/UI/uCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <slot name="icon" class="icon-bg"></slot>
+      <slot name="header"></slot>
+    </div>
+    <div class="card-body">
+      <slot></slot>
+    </div>
+    <div class="card-footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.card {
+  border: 1px solid var(--primary);
+  border-radius: 0.5rem;
+  padding: 1rem;
+}
+:slotted(svg) {
+  fill: #c5c5c5;
+  background-color: #303030;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+}
+</style>
