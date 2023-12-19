7dbab47946ff694dbc25fda0c4958c8c81b19c0a
DevKing472 <Fundorashweta472@gmail.com>
2023-12-19 21:15:41 +0000

    Revert type ignore

--- src/components/Icons/VueIcon.vue
+++ src/components/Icons/VueIcon.vue
@@ -31,6 +31,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+// @ts-ignore
 import { useMotionProperties, useSpring, type PermissiveMotionProperties } from '@vueuse/motion'
 import type { DragHandler } from '@/types/gesture.types'
 
