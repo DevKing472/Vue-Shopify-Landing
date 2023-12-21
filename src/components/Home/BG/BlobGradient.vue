7bdf4ef6f64a027ed2bd601c566920b939c613bc
DevKing472 <Fundorashweta472@gmail.com>
2023-12-21 01:12:08 +0000

    Fix all issues

--- src/components/Home/BG/BlobGradient.vue
+++ src/components/Home/BG/BlobGradient.vue
@@ -1,8 +1,21 @@
 <template>
-  <img src="../../../assets/images/blog_grad.webp" class="blob" />
+  <img :src="image" class="blob" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark } from '@vueuse/core'
+import { computed } from 'vue'
+import BlogGradDark from '../../../assets/images/blog_grad.webp'
+import BlogGradLight from '../../../assets/images/mesh_grad_bg_light.png'
+const isDark = useDark()
+const image = computed(() => {
+  return isDark.value ? BlogGradDark : BlogGradLight
+})
+
+const opacityVal = computed(() => {
+  return isDark.value ? '0.25' : '0.05'
+})
+</script>
 
 <style scoped>
 .blob {
@@ -11,8 +24,8 @@
   height: 800px;
   top: 10%;
   z-index: -1;
-  opacity: 0.2;
+  opacity: v-bind('opacityVal');
   border-radius: 50% 50% 0 0;
-  filter: blur(10px);
+  filter: blur(10px) brightness(0.5);
 }
 </style>
